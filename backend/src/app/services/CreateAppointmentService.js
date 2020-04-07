d20f37da9880b44b68ccbacfe59923bdd2a3c05c
Dtesch9 <alucard.s8123@gmail.com>
2020-04-07 09:57:24 -0300

    Cache

--- backend/src/app/services/CreateAppointmentService.js
+++ backend/src/app/services/CreateAppointmentService.js
@@ -6,6 +6,8 @@ import Appointment from '../models/Appointment';
 
 import Notification from '../schemas/Notification';
 
+import Cache from '../../lib/Cache';
+
 class CreateAppointmentService {
   async run({ provider_id, user_id, date }) {
     if (provider_id === user_id) {
@@ -67,6 +69,11 @@ class CreateAppointmentService {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    /**
+     * Invalidate cache
+     */
+    await Cache.invalidatePrefix(`user:${user.id}:appointments`);
 
     return appointment;
   }
